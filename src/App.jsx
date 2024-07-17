98e909cb0269b68c7e041e5b705ed424108b9446
palmergk <palmergid@gmail.com>
2024-07-17 18:23:44 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,23 @@ import UsersComponent from './UsersComponent'
 
 const App = () => {
 
+
   const [form, setForm] = useState({
     username: "",
     email: "",
-    phone: ""
+    phone: "",
   })
+
 
   const HandleForm = (event) => {
     setForm({
       ...form,
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     })
   }
 
   const [userArray, setUserArray] = useState([])
+  const [altUserArray, setAltUserArray] = useState([])
   const [error, setError] = useState('')
 
   // const Array = [
@@ -36,9 +39,20 @@ const App = () => {
     if (!form.email) return setError('Enter fields correctly')
     if (!form.phone) return setError('Enter fields correctly')
 
+    const date = new Date()
+    const formdata = {
+      ...form,
+      id: date.getTime(),
+    }
+
     setUserArray([
       ...userArray,
-      form
+      formdata
+    ])
+
+    setAltUserArray([
+      ...altUserArray,
+      formdata
     ])
 
     setForm({
@@ -55,15 +69,18 @@ const App = () => {
     <div className='w-3/4 mx-auto my-10 '>
       <div className='flex items-center gap-4 mx-auto relative w-fit'>
         <div className='flex flex-col gap-4'>
-          <input type='text' className='border outline-none p-1.5 text-sm w-64 border-zinc-300' name='username' value={form.username} onChange={HandleForm} />
-          <input type='email' className='border outline-none p-1.5 text-sm w-64 border-zinc-300' name='email' value={form.email} onChange={HandleForm} />
-          <input type='text' className='border outline-none p-1.5 text-sm w-64 border-zinc-300' name='phone' value={form.phone} onChange={HandleForm} />
+          <input type='text' className='border outline-none p-1.5 text-sm w-64 border-zinc-300' name='username' value={form.username} placeholder='Enter a username' onChange={HandleForm} />
+          <input type='email' className='border outline-none p-1.5 text-sm w-64 border-zinc-300' name='email' value={form.email} placeholder='Enter a email' onChange={HandleForm} />
+          <input type='text' className='border outline-none p-1.5 text-sm w-64 border-zinc-300' name='phone' value={form.phone} placeholder='Enter a phone number' onChange={HandleForm} />
         </div>
-        <button className='bg-yellow-400 px-6 py-2 rounded-lg font-medium text-white capitalize' onClick={AddUsers}>add</button>
+        <button className='bg-yellow-800 px-6 py-2 rounded-lg font-medium text-white capitalize' onClick={AddUsers}>add</button>
         <div className='text-xs text-red-500 text-center absolute -bottom-6 left-0'>{error}</div>
       </div>
       <UsersComponent
         userArray={userArray}
+        setUserArray={setUserArray}
+        altUserArray={altUserArray}
+        setAltUserArray={setAltUserArray}
       />
     </div>
   )
--- src/UsersComponent.jsx
+++ src/UsersComponent.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const UsersComponent = ({userArray}) => {
+const UsersComponent = ({userArray, altUserArray, setAltUserArray, setUserArray}) => {
+    const [search, setSearch] = useState('')
+    
+
+    const HandleSearch = () => {
+        if(search){
+            const searchFilter = altUserArray.filter(object => object.username.includes(search.toLowerCase()) || object.email.includes(search.toLowerCase()) )
+            setAltUserArray(searchFilter)
+        }else {
+            setAltUserArray(userArray)
+        }
+    }
+
+    const DeleteObjects = (id) => {
+       const nonDeletedObjects = altUserArray.filter(object => object.id !== id)
+       setAltUserArray(nonDeletedObjects)
+       setUserArray(nonDeletedObjects)
+    }
+
     return (
-        <div className='mt-16 bg-white h-[75vh] p-8 shadow-2xl w-1/2 mx-auto rounded-md overflow-y-auto scroll'>
+        <div className='mt-16 bg-white h-[75vh] p-8 shadow-2xl w-7/12 mx-auto rounded-md overflow-y-auto scroll '>
             <div className='border-b text-xl font-bold text-center uppercase'>The company users</div>
+            <div className='mt-4 flex items-center justify-center'>
+                <input className='w-60 h-fit px-2 py-1 border border-zinc-500 text-sm rounded-sm outline-none' value={search} placeholder='search by username or email' onChange={(event) => setSearch(event.target.value)} onKeyUp={HandleSearch}></input>
+            </div>
             <div className='flex flex-col gap-6 mt-10'>
-                {userArray.map((ele, index) => (
+                {altUserArray.map((ele, index) => (
                     <div key={index} className='w-full border h-fit p-2 flex flex-col gap-2'>
                         <div className='flex justify-between text-sm capitalize'>
                             <div className='font-medium'>Username:</div>
@@ -18,6 +39,9 @@ const UsersComponent = ({userArray}) => {
                         <div className='flex justify-between text-sm '>
                             <div className='font-medium'>Phone:</div>
                             <div>{ele.phone}</div>
+                        </div>
+                        <div className='ml-auto'>
+                            <button className='py-1 px-3 text-xs capitalize rounded-md bg-yellow-800 text-white' onClick={() => DeleteObjects(ele.id)}>delete</button>
                         </div>
                     </div>
                 ))}
